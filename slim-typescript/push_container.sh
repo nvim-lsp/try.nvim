68dc0235492d83c6f22bb5b2eeab26864371ec74
Michael Lingelbach <m.j.lbach@gmail.com>
2021-10-21 10:47:38 -0700

    fix: adjust slim-typescript push url

--- slim-typescript/push_container.sh
+++ slim-typescript/push_container.sh
@@ -1,2 +1,2 @@
 podman build -t try.nvim:slim-typescript .
-podman push try.nvim:defaults docker.io/mjlbach/try.nvim:slim-typescript
+podman push try.nvim:slim-typescript docker.io/mjlbach/try.nvim:slim-typescript
