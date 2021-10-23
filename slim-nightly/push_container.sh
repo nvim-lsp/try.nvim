f77d342f544bed482b0b17b105f39dca6a6cb438
Jan Schmitt <jan@smittie.de>
2021-10-23 19:56:17 +0200

    feat: dedicated base images for nightly and stable

---
+++ slim-nightly/push_container.sh
@@ -0,0 +1,2 @@
+podman build -t try.nvim:slim-nightly .
+podman push try.nvim:slim-nightly docker.io/nvimlsp/try.nvim:slim-nightly
---
+++ slim-stable/push_container.sh
@@ -0,0 +1,2 @@
+podman build -t try.nvim:slim-stable .
+podman push try.nvim:slim-stable docker.io/nvimlsp/try.nvim:slim-stable
