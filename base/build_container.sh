db390f4345d01331a08dbba6ff2608355d9d6efc
Michael Lingelbach <m.j.lbach@gmail.com>
2021-10-29 18:53:19 -0700

    fix: use correct nightly/release branches

--- base/build_container.sh
+++ base/build_container.sh
@@ -1,2 +1,2 @@
-podman build -t try.nvim:base-stable --build-arg VERSION=stable .
-podman build -t try.nvim:base-nightly .
+podman build -t try.nvim:base-stable --build-arg VERSION=release-0.5 .
+podman build -t try.nvim:base-nightly --build-arg VERSION=master .
