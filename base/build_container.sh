5b8d1013c13ce0384b20cfd1317ed91a11faebbb
Michael Lingelbach <m.j.lbach@gmail.com>
2021-11-30 15:29:24 -0500

    chore: bump to release-0.6

--- base/build_container.sh
+++ base/build_container.sh
@@ -1,2 +1,2 @@
-podman build -t try.nvim:base-stable --build-arg VERSION=release-0.5 .
+podman build -t try.nvim:base-stable --build-arg VERSION=release-0.6 .
 podman build -t try.nvim:base-nightly --build-arg VERSION=master .
