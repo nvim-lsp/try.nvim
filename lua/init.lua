257b486bc4fd4ac574e8ebcf4e33e99439fdee3c
Michael Lingelbach <m.j.lbach@gmail.com>
2022-02-09 23:30:28 -0800

    fix: nightly start rtp

--- lua/init.lua
+++ lua/init.lua
@@ -3,7 +3,7 @@ local install_path = vim.fn.stdpath 'data' .. '/site/pack/packer/start/packer.nv
 
 if vim.fn.empty(vim.fn.glob(install_path)) > 0 then
   vim.fn.execute('!git clone https://github.com/wbthomason/packer.nvim ' .. install_path)
-  vim.cmd [[ packadd packer ]]
+  vim.o.runtimepath = vim.fn.stdpath('data') .. '/site/pack/*/start/*,' .. vim.o.runtimepath
 end
 
 vim.cmd [[
