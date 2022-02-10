52009cc4edcc0d35ef82850f4a409b562474e451
Michael Lingelbach <m.j.lbach@gmail.com>
2022-02-09 23:24:36 -0800

    fix: neovim nightly rtp and cleanups

--- elixir/init.lua
+++ elixir/init.lua
@@ -3,6 +3,7 @@ local install_path = vim.fn.stdpath 'data' .. '/site/pack/packer/start/packer.nv
 
 if vim.fn.empty(vim.fn.glob(install_path)) > 0 then
   vim.fn.execute('!git clone https://github.com/wbthomason/packer.nvim ' .. install_path)
+  vim.cmd [[ packadd packer ]]
 end
 
 vim.cmd [[
